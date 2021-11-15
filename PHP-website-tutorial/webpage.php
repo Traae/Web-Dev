126f0029ceaaf6389ca97949fda0937ce7f45e33
Austin <epidermus>
2021-11-14 17:17:01 -0700

    added a for loop to show lightly how they work

--- PHP-website-tutorial/webpage.php
+++ PHP-website-tutorial/webpage.php
@@ -5,5 +5,12 @@
  <!DOCTYPE HTML>
  <html>
  <h1>This is a test Website</h1>
- <?php echo("This is a test"); ?>
+
+ <?php
+  // you can use the <?php to type php that does responsive things
+  for ($i=0; $i < 5; $i++) {
+    echo("This is a test");
+  }
+
+  ?>
  </html>
